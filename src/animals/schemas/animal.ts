f6f4edb29d70edee0fd881459c7701cd1541f5b6
Yevestevez <yevestevez.cg@gmail.com>
2026-04-18 18:44:39 +0200

    Refactor Animal schema to use string for ID and update DTO definitions

--- src/animals/schemas/animal.ts
+++ src/animals/schemas/animal.ts
@@ -1,15 +1,30 @@
 import * as z from 'zod';
 
 export const AnimalSchema = z.object({
-    id: z.uuid(),
-    //weight: z.coerce.number(),
-    //price: z.coerce.number(),
-    //color: z.string().optional(),
-    //owner: z.string().optional(),
+    id: z.string(),
+    name: z.string().nonempty(),
+    englishName: z.string().nonempty(),
+    sciName: z.string().nonempty(),
+    group: z.string().nonempty(),
+    image: z.string().url(),
+    diet: z.string(),
+    lifestyle: z.enum(['Diurno', 'Nocturno']),
+    location: z.string(),
+    slogan: z.string(),
 });
 
-//export const AnimalSchemaDTO = AnimalSchema.omit({ id: true });
+export const AnimalSchemaDTO = z.object({
+    name: z.string().nonempty(),
+    englishName: z.string().nonempty(),
+    sciName: z.string().nonempty(),
+    group: z.string().nonempty(),
+    image: z.string().url(),
+    diet: z.string(),
+    lifestyle: z.enum(['Diurno', 'Nocturno']),
+    location: z.string(),
+    slogan: z.string(),
+});
 
 export type Animal = z.infer<typeof AnimalSchema>;
-//export type AnimalDTO = z.infer<typeof AnimalSchemaDTO>;
-//export type AnimalUpdateDTO = Partial<AnimalDTO>;
+export type AnimalDTO = Omit<Animal, 'id'>;
+export type AnimalUpdateDTO = Partial<Omit<Animal, 'id'>>;
